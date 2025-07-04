8a0569c263a19690fb96402390e827ad050fc201
collins <edepaul148@gmail.com>
2025-07-03 19:13:14 -0700

    start server function

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,27 @@ import { connectDB } from "./config/db.js";
 
 const app = express();
 
-connectDB();
+
 
 app.get("/",(req,res)=> res.send("Hello server"))
 
 
 
-app.listen(ENV.PORT, ()=> console.log("server is up and running good on PORT:", ENV.PORT));
+// app.listen(ENV.PORT, ()=> console.log("server is up and running good on PORT:", ENV.PORT));
+
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        
+        app.get("/", (req, res) => res.send("Hello server"))
+        
+        app.listen(ENV.PORT, () => console.log("server is up and running good on PORT:", ENV.PORT));
+    } catch (error) {
+        console.error("Failed to start server:", error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
